aa49198fbb4e4d5af13c42b23319d180b4068af2
Simon Tharby <simon@liquidvoting.io>
2021-07-02 13:47:45 +0100

    Example chart with points and line series

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,43 @@
 use plotters::prelude::*;
-use plotters::coord::types::RangedCoordf32;
-
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let root = BitMapBackend::new("images/4.png", (640, 480)).into_drawing_area();
+    let root = BitMapBackend::new("images/5.png", (640, 640)).into_drawing_area();
+    root.fill(&WHITE);
+    let root = root.margin(10, 10, 10, 10);
+    // After this point, we should be able to draw construct a chart context
+    let mut chart = ChartBuilder::on(&root)
+        // Set the caption of the chart
+        .caption("This is our first plot", ("sans-serif", 40).into_font())
+        // Set the size of the label region
+        .x_label_area_size(20)
+        .y_label_area_size(40)
+        // Finally attach a coordinate on the drawing area and make a chart context
+        .build_cartesian_2d(0f32..10f32, 0f32..10f32)?;
 
-    root.fill(&RGBColor(240, 200, 200))?;
+    // Then we can draw a mesh
+    chart
+        .configure_mesh()
+        // We can customize the maximum number of labels allowed for each axis
+        .x_labels(5)
+        .y_labels(5)
+        // We can also change the format of the label text
+        .y_label_formatter(&|x| format!("{:.3}", x))
+        .draw()?;
 
-    let root = root.apply_coord_spec(Cartesian2d::<RangedCoordf32, RangedCoordf32>::new(
-        0f32..1f32,
-        0f32..1f32,
-        (0..640, 0..480),
-    ));
-
-    let dot_and_label = |x: f32, y: f32| {
-        return EmptyElement::at((x, y))
-            + Circle::new((0, 0), 3, ShapeStyle::from(&BLACK).filled())
-            + Text::new(
-                format!("({:.2},{:.2})", x, y),
-                (10, 0),
-                ("sans-serif", 15.0).into_font(),
-            );
-    };
-
-    root.draw(&dot_and_label(0.5, 0.6))?;
-    root.draw(&dot_and_label(0.25, 0.33))?;
-    root.draw(&dot_and_label(0.8, 0.8))?;
-
+    // And we can draw something in the drawing area
+    chart.draw_series(LineSeries::new(
+        vec![(0.0, 0.0), (5.0, 5.0), (8.0, 7.0)],
+        &RED,
+    ))?;
+    // Similarly, we can draw point series
+    chart.draw_series(PointSeries::of_element(
+        vec![(0.0, 0.0), (5.0, 5.0), (8.0, 7.0)],
+        5,
+        &RED,
+        &|c, s, st| {
+            return EmptyElement::at(c)    // We want to construct a composed element on-the-fly
+            + Circle::new((0,0),s,st.filled()) // At this point, the new pixel coordinate is established
+            + Text::new(format!("{:?}", c), (10, 0), ("sans-serif", 10).into_font());
+        },
+    ))?;
     Ok(())
 }
